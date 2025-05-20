373fb7ec4fc09b4db430de228df692a7746b306a
RalfsAizsils <ralfs.aizsils@edu.rtu.lv>
2025-05-20 06:06:47 +0300

    .

--- PP_LD3.cpp
+++ PP_LD3.cpp
@@ -7,25 +7,56 @@
 #include <chrono>
 #include <cmath>
 #ifdef __linux__ 
-	#include <CL/cl.hpp>
+#include <CL/cl.hpp>
 #else
-	#include <CL/opencl.hpp>
+#include <CL/opencl.hpp>
 #endif
+#pragma pack(1)
 
-cl::Program setup() {
+using namespace std;
+using namespace cl;
+
+CommandQueue queue;
+Kernel kernel;
+Context context;
+Program program;
+cl_float4* cpu_output;
+Buffer cl_output;
+
+// CONFIGURATION
+#define IMG_WIDTH 1920
+#define IMG_HEIGHT 1080
+#define LOCAL_WORK_SIZE 64
+
+struct Shape {
+	cl_float3 scale;
+	cl_float3 pos;
+	cl_float3 color;
+	int shape_type = 0; // 0 = sphere, 1 = plane, 2 = cilinder
+	float f4, f5, f6; // mem spacing to align to opencl shape struct
+};
+
+struct Light {
+	cl_float3 color;
+	cl_float3 dir;
+};
+
+void setup_opencl() {
+	std::cout << "Setting up OpenCL...\n";
 	std::vector<cl::Platform> platforms;
 	cl::Platform::get(&platforms);
 	auto platform = platforms[0];
 	std::vector<cl::Device> devices;
 	platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
 	auto device = devices[0];
-
-	std::ifstream matecarlo("matecarlo.cl");
-	std::string src(std::istreambuf_iterator<char>(matecarlo), (std::istreambuf_iterator<char>()));
-	cl::Program::Sources sources(1, src.c_str());
-	cl::Context context(devices);
-	cl::Program program(context, sources);
-	auto err = program.build("-cl-std=CL3.0");
+	std::ifstream renderer("renderer.cl");
+	std::string src(std::istreambuf_iterator<char>(renderer), (std::istreambuf_iterator<char>()));
+	Program::Sources sources(1, src.c_str());
+	context = Context(devices);
+	program = Program(context, sources);
+	queue = CommandQueue(context, device);
+	auto err = program.build({ device });
+	kernel = Kernel(program, "render_kernel"); 
 
 	if (err == CL_BUILD_PROGRAM_FAILURE) {
 		size_t log_size;
@@ -34,82 +65,97 @@ cl::Program setup() {
 		clGetProgramBuildInfo(program(), devices[0](), CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
 		printf("%s\n", log);
 	}
-
-	return program;
 }
 
-float func(float x, int a, int b, int c) { return (x - a) * (x - b) * (x - c); }
-
-float montecarlo(
-	int xmin, int xmax,
-	int ymin, int ymax,
-	int a, int b, int c,
-	const size_t points
-) {
-	signed long long int count = 0;
-	float x, y, f;
-	for (int i = 0; i < points; i++) {
-		y = ymin + static_cast<float>(rand()) / RAND_MAX * (ymax - ymin);
-		x = xmin + static_cast<float>(rand()) / RAND_MAX * (xmax - xmin);
-		f = func(x, a, b, c);
-		if (f > 0 && y < f && y > 0) count++;
-		if (f < 0 && y > f && y < 0) count--;
-	}
-	return (float)count / (float)points * (xmax - xmin) * (ymax - ymin);
-}
-
-float opencl_montecarlo(
-	cl::Program program, cl::Context context, cl::Device device,
-	int xmin, int xmax,
-	int ymin, int ymax,
-	int a, int b, int c,
-	const int points
-) {
-	int power_per_unit = std::max(points / CL_DEVICE_MAX_COMPUTE_UNITS,1);
-	std::vector<int> buff_out(CL_DEVICE_MAX_COMPUTE_UNITS);
-	std::vector<int> params = { a, b, c, xmin, xmax, ymin, ymax, power_per_unit };
-	cl::Buffer outBuffer(context, CL_MEM_WRITE_ONLY, sizeof(int) * buff_out.size(), nullptr);
-	cl::Buffer paramBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(int) * params.size(), params.data()); 
-	cl::Kernel kernel(program, "matecarlo_fun");
-	kernel.setArg(0, outBuffer);
-	kernel.setArg(1, paramBuffer); 
-
-	auto name = device.getInfo<CL_DEVICE_NAME>();
-	std::cout << "Device: " << name << " " << CL_DEVICE_MAX_COMPUTE_UNITS << "\n";
-
-	cl::CommandQueue queue(context, device);  
-	queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(buff_out.size()));
-	queue.enqueueReadBuffer(outBuffer, CL_TRUE, 0, sizeof(float) * buff_out.size(), buff_out.data());
-
+void render_image(cl_float3 sky_color, Light light, Shape* shapes, int shape_count)
+{
+	std::cout << "Rendering...\n";
+	cpu_output = new cl_float3[IMG_WIDTH * IMG_HEIGHT];
+	cl_output = Buffer(context, CL_MEM_WRITE_ONLY, IMG_WIDTH * IMG_HEIGHT * sizeof(cl_float3));
+	Buffer shape_buffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(Shape) * shape_count, shapes);
+	kernel.setArg(0, cl_output);
+	kernel.setArg(1, IMG_WIDTH);
+	kernel.setArg(2, IMG_HEIGHT);
+	kernel.setArg(3, sky_color);
+	kernel.setArg(4, shape_buffer);
+	kernel.setArg(5, shape_count);
+	kernel.setArg(6, light);
+	queue.enqueueNDRangeKernel(kernel, NULL, IMG_WIDTH * IMG_HEIGHT, LOCAL_WORK_SIZE);
+	//queue.finish();
+	queue.enqueueReadBuffer(cl_output, CL_TRUE, 0, IMG_WIDTH * IMG_HEIGHT * sizeof(cl_float3), cpu_output);
 	queue.finish();
+}
 
-	float count = 0.0f;
-	for (int v : buff_out) count += v;
-	return (float)count / (float)(points) * (xmax - xmin) * (ymax - ymin);;
+inline float clamp(float x) { return x < 0.0f ? 0.0f : x > 1.0f ? 1.0f : x; }
+inline int toInt(float x) { return int(clamp(x) * 255 + .5); }
+void saveImage() { // save image to file .PPM
+	std::cout << "Saving image...\n";
+	FILE* f = NULL;
+	if (fopen_s(&f, "_res.ppm", "w") != 0) {
+		perror("Failed to open file");
+		return;
+	}
+	fprintf(f, "P3\n%d %d\n%d\n", IMG_WIDTH, IMG_HEIGHT, 255);
+
+	for (int i = 0; i < IMG_WIDTH * IMG_HEIGHT; i++) {
+		//printf("%d %d %d ", toInt(cpu_output[i].s[0]), toInt(cpu_output[i].s[1]), toInt(cpu_output[i].s[2]));
+		fprintf(f, "%d %d %d ", toInt(cpu_output[i].s[0]), toInt(cpu_output[i].s[1]), toInt(cpu_output[i].s[2]));
+	}
+}
+
+cl_float3 make_float3(float x = 0, float y = 0, float z = 0) {
+	cl_float3 f;
+	f.s[0] = x;
+	f.s[1] = y;
+	f.s[2] = z;
+	f.s[3] = 0.0f; 
+	return f;
 }
 
 int main() {
-	#if CL_DEVICE_IMAGE_SUPPORT == CL_FALSE 
-		std::cout << "Images not supported on this OpenCL version.\n";
-		return 0;
-	#endif
+#if CL_DEVICE_IMAGE_SUPPORT == CL_FALSE 
+	std::cout << "Images not supported on this OpenCL version.\n";
+	return 0;
+#endif
 
 	std::cout << "Starting...\n";
 
 	long long elapsed = 0;
 	std::chrono::system_clock::time_point start, end;
 
-	cl::Program program = setup();
-	cl::Context context = program.getInfo<CL_PROGRAM_CONTEXT>();
-	cl::Device device = program.getInfo<CL_PROGRAM_DEVICES>()[0];
+	setup_opencl();
 
-	/*std::cout << "\nParalel code with 1000 points:\n";
-	start = std::chrono::system_clock::now();
-	res = opencl_montecarlo( program, context, device, x_min, x_max, y_min, y_max, a, b, c, 1000);
-	printf("Definite integral = %.5f\n", res);
-	end = std::chrono::system_clock::now();
-	elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-	std::cout << "Elapsed time = " << elapsed << "ms\n";*/
+	Light sun;
+	sun.color = make_float3(1.0f, 1.0f, 1.0f);
+	sun.dir = make_float3(0.0f, 0.0f, 1.0f);
+	Shape* shapes  = new Shape[5];
 
-	std::cin.get();
+	shapes[0].scale = make_float3(0.5f);
+	shapes[0].pos = make_float3(0.0f, 0.0f, -100.0f);
+	shapes[0].color = make_float3(0.75f, 0.0f, 0.05f);
+	shapes[0].shape_type = 0;
+	shapes[1].scale = make_float3(0.2f);
+	shapes[1].pos = make_float3(-0.5f, -0.2f, -80.0f);
+	shapes[1].color = make_float3(0.1f, 0.1f, 0.9f);
+	shapes[1].shape_type = 0;
+	shapes[2].scale = make_float3(0.2f);
+	shapes[2].pos = make_float3(0.5f, 0.3f, -120.0f); 
+	shapes[2].color = make_float3(0.1f, 0.1f, 0.85f);
+	shapes[2].shape_type = 0;
+	shapes[3].scale = make_float3(0.5f);
+	shapes[3].pos = make_float3(1.5f, 0.0f, -150.0f);
+	shapes[3].color = make_float3(1.0f, 1.0f, 1.0f);
+	shapes[3].shape_type = 2;
+	shapes[4].scale = make_float3(5.0f, 5.0f);
+	shapes[4].pos = make_float3(0.0f,-1.2f,-100.0f);
+	shapes[4].color = make_float3(0.1f, 0.1f, 0.1f);
+	shapes[4].shape_type = 1;
+
+	render_image(make_float3(0.8f, 0.8f, 0.8f), sun, shapes, 5); 
+	//render_image(make_float3(0.2f, 0.2f, 1.0f));
+	saveImage();
+
+	delete[] cpu_output;
+	std::cout << "Done!\n";
 }
+
--- renderer.cl
+++ renderer.cl
@@ -1,9 +1,111 @@
-__kernel void render_kernel(__global float3* output, int width, int height)
+struct Ray{
+	float3 origin;
+	float3 dir;
+};
+
+struct Light{
+	float3 color;
+	float3 dir;
+};
+
+struct Shape{
+	float3 scale;
+	float3 pos; // left/right, up/down, forward/backward
+	float3 color;
+	int shape_type; // 0 = sphere, 1 = plane, 2 = cilinder
+};
+
+struct Ray createCamRay(const int x_coord, const int y_coord, const int width, const int height){
+	float fx = (float)x_coord / (float)width;
+	float fy = (float)y_coord / (float)height;
+	float fx2 = (fx - 0.5f) * ((float)(width) / (float)(height));
+	float fy2 = fy - 0.5f;
+	float3 pixel_pos = (float3)(fx2, -fy2, 0.0f);
+	struct Ray ray;
+	ray.origin = (float3)(0.0f, 0.0f, 70.0f);
+	ray.dir = normalize(pixel_pos - ray.origin);
+
+	return ray;
+}
+
+bool intersect_shape(const struct Shape* shape, const struct Ray* ray, float* t) {
+	float l = ray->dir.x;
+	float m = ray->dir.y;
+	float n = ray->dir.z;
+	float x = ray->origin.x - shape->pos.x;
+	float y = ray->origin.y - shape->pos.y;
+	float z = ray->origin.z - shape->pos.z;
+
+	if(shape->shape_type == 0) { // sphere
+		float A = l * l + m * m + n * n;
+		float B = 2.0f * (l * x + m * y + n * z);
+		float C = x * x + y * y + z * z - shape->scale.x * shape->scale.x;
+
+		float D = B * B - 4.0f * A * C;
+		if (D < 0.0f) return false;
+
+		float t1 = (-B - sqrt(D)) / (2.0f * A);
+		//float t2 = (-B + sqrt(D)) / (2.0f * A);
+		if (t1 > 0.0f && t1 < *t) {
+			*t = t1;
+			return true;
+		}
+
+	} else if(shape->shape_type == 1) { // inf plane
+		float D = shape->pos.y;
+		float A = 0;
+		float B = 1;
+		float C = 0;
+
+		float t0 = (D - (A * ray->origin.x + B * ray->origin.y + C * ray->origin.z)) / (l * A + m * B + n * C);
+		if (t0 > 0.0f && t0 < *t) {
+			*t = t0;
+			return true;
+		}
+
+	} else if(shape->shape_type == 2) { // cilinder
+		float A = l * l + n * n;
+		float B = 2.0f * (l * x + n * z);
+		float C = x * x + z * z - shape->scale.x * shape->scale.x;
+
+		float D = B * B - 4.0f * A * C;
+		if (D < 0.0f) return false;
+
+		float t1 = (-B - sqrt(D)) / (2.0f * A);
+		//float t2 = (-B + sqrt(D)) / (2.0f * A);
+		if (t1 > 0.0f && t1 < *t) {
+			*t = t1;
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+__kernel void render_kernel(__global float3* output, int width, int height, float3 bg_color, __global const struct Shape* world_geomerty, int world_geo_size, struct Light sun)
 {
-	 const int work_item_id = get_global_id(0); /* the unique global id of the work item for the current pixel */
-	 int x = work_item_id % width; /* x-coordinate of the pixel */
-	 int y = work_item_id / width; /* y-coordinate of the pixel */
-	 float fx = (float)x / (float)width; /* convert int to float in range [0-1] */
-	 float fy = (float)y / (float)height; /* convert int to float in range [0-1] */
-	 output[work_item_id] = (float3)(fx, fy, 0); /* simple interpolated colour gradient based on pixel coordinates */
+	struct Shape shape = world_geomerty[2];
+	//printf("shape %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %d\n", shape.scale.x, shape.scale.y, shape.scale.z, shape.pos.x, shape.pos.y, shape.pos.z, shape.color.x, shape.color.y, shape.color.z, shape.shape_type);
+	const int gid = get_global_id(0);
+	int x = gid % width;
+	int y = gid / width;
+
+	//float fx = (float)x / (float)width;
+	//float fy = (float)y / (float)height;
+	//output[gid] = (float3)(fx, fy, 0);
+
+	struct Ray camray = createCamRay(x, y, width, height);
+
+	float t = 1e20;
+	float3 hit_color = bg_color;
+	for(int i = 0; i < world_geo_size; i++) {
+		struct Shape shape = world_geomerty[i];
+		//printf("shape %.1f %.1f %.1f %.1f %.1f %.1f\n", shape.pos.x, shape.pos.y, shape.pos.z, shape.color.x, shape.color.y, shape.color.z);
+		if (intersect_shape(&shape, &camray, &t)) {
+			hit_color = shape.color;
+		}
+	}
+
+	output[gid] = hit_color;
+
 }
